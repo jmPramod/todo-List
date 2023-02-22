445790a83824c7d6e22d649f8aef4d418a2ee91c
pramodJ <pramodjm4@gmail.com>
2023-02-22 18:45:11 +0530

    #

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -24,7 +24,10 @@ const Todo = ({ list, setList, setEdittodo }) => {
 
   return (
     <div className="allList">
-      {list.map((v, i) => (
+  {list.length<=0 && <h1 style={{textAlign:"center"}}>nothing to display</h1>}
+  
+  
+     {list.map((v, i) => (
         <>
           <li
             key={v.id}
